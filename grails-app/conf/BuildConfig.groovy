499373bf549ce1d9c723a2a562b769e12c588f9e
mbohun <martin.bohun@gmail.com>
2014-10-09 17:10:08 +1100

    desperado continues: trying the old maven-release plugin 2.2.0 with export=false

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -41,6 +41,9 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
+        build ':release:2.2.0', ':rest-client-builder:1.0.3', {
+	    export = false
+        }
         runtime ":jquery:1.8.3"
         runtime ":resources:1.1.6"
         runtime ":ala-web-theme:0.2.2"
